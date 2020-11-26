ab312ddbc7df27271ca4a358cdbf2f463d29bae5
Aamir Soni <aamirsoni1551@gmail.com>
2020-11-26 21:54:50 +0000

    mouse controls added

--- render/cube_render.cpp
+++ render/cube_render.cpp
@@ -12,6 +12,18 @@
 
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+float deltaTime = 0.0f;         // Time between current frame and last frame
+float lastFrame = 0.0f;         // Time of last frame
+float lastX = 400, lastY = 300; //needed for mouse initial values
+
+bool firstMouse = false;
+float yaw = -90.0f;
+float pitch = 0.0f;
 
 int main()
 {
@@ -36,9 +48,10 @@ int main()
     }
 
     glViewport(0, 0, 800, 600);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    Shader ourShader("../shaders/vert.glsl", "../shaders/frag.glsl");
-    ourShader.use();
+    Shader shader("../shaders/vert.glsl", "../shaders/frag.glsl");
+    shader.use();
 
     unsigned int vao, vbo, ebo;
     glGenVertexArrays(1, &vao); // this only gives a number to the vao varialbe, doesnt acc create a buffer
@@ -47,7 +60,10 @@ int main()
 
     glm::mat4 id = glm::mat4(1.0f);
 
-    glm::mat4 view = glm::translate(id, glm::vec3(0.0f, 0.0f, -4.0f)); //translating the scene in reverse direction
+    glm::mat4 view;
+    view = glm::lookAt(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    //camera position, camera target, world space up
+
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
     glm::vec3 positions[] = {
@@ -62,7 +78,7 @@ int main()
         glm::vec3(1.5f, 0.2f, -1.5f),
         glm::vec3(-1.3f, 1.0f, -1.5f)};
 
-    Cubes::InitializeCube(1.0f, vao, vbo, ebo, &view, &projection, ourShader.shader_id);
+    Cubes::InitializeCube(1.0f, vao, vbo, ebo, &view, &projection, shader.shader_id);
 
     Cubes::AddVerticesToBuffers();
 
@@ -71,12 +87,17 @@ int main()
     glBindVertexArray(vao);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    glUseProgram(ourShader.shader_id);
+    glUseProgram(shader.shader_id);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     while (!glfwWindowShouldClose(window))
     { // render loop -- an iteration of this main render loop is called a frame
         processInput(window);
+        glfwSetCursorPosCallback(window, mouse_callback);
+
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -85,6 +106,9 @@ int main()
             glm::mat4 model = glm::translate(id, positions[i]);
             model = glm::rotate(model, glm::radians((float)glfwGetTime() * 20 * (i + 1)), glm::vec3(0.2f, 0.8f, 0.3f));
             cubes.models.push_back(&model);
+
+            view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
             cubes.ApplyUniforms(i);
 
             glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
@@ -114,4 +138,44 @@ void processInput(GLFWwindow *window)
     {
         glfwSetWindowShouldClose(window, true);
     }
+
+    float cameraSpeed = 4.0f * deltaTime; //change 2.5 if you want it to move at a different speed
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) *
+                     cameraSpeed; // use right hand rule to figure this out
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) *
+                     cameraSpeed;
+}
+
+void mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+    yaw += xoffset;
+    pitch += yoffset;
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
 }
