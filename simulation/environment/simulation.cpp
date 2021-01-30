22a32c31a3c2a1d54bcc16ac91eb072682957afd
Aamir <aamirsoni1551@gmail.com>
2021-01-30 23:13:31 +0000

    fixing bugs

--- simulation/environment/simulation.cpp
+++ simulation/environment/simulation.cpp
@@ -33,6 +33,7 @@ int main()
 
     real cube_length = 4.0f;
     Camera camera = Camera(world_properties->window);
+    camera.camera_pos = glm::vec3(0, 0, 150);
     BlockRenderer::InitialiseBlockRenderer(&camera, cube_length, vao, vbo, ebo, world_properties);
 
     int num_blocks_same = 40;
