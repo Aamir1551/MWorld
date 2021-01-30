c552e5a61a8575abc5165e8ae7daef03688f84b8
Aamir <aamirsoni1551@gmail.com>
2021-01-30 17:06:44 +0000

    add index matrix function

--- simulation/environment/world_handler.cpp
+++ simulation/environment/world_handler.cpp
@@ -36,18 +36,6 @@ std::vector<Matrix> *GeneratePositions(int num_cubes)
     return positions;
 }
 
-std::vector<Matrix> *GenerateAngularMomentums(int num_cubes)
-{
-    auto *angular_momentums = new std::vector<Matrix>;
-    auto get_angular_momentums = []() -> real { return (rand() % 1)/100 - 0.0005;};
-    for (int i = 0; i < num_cubes; i++)
-    {
-        real values[] = {get_angular_momentums(), get_angular_momentums(), get_angular_momentums()};
-        angular_momentums->push_back(Matrix(3, 1, values));
-    }
-    return angular_momentums;
-}
-
 std::vector<Matrix> *GenerateLinearMomentums(int num_cubes)
 {
     auto *linear_momentums = new std::vector<Matrix>;
@@ -58,17 +46,6 @@ std::vector<Matrix> *GenerateLinearMomentums(int num_cubes)
         linear_momentums->push_back(Matrix(3, 1, values));
     }
     return linear_momentums;
-}
-
-std::vector<Quaternion> *GenerateOrientations(int num_cubes)
-{
-    auto *orientations = new std::vector<Quaternion>;
-    for (int i = 0; i < num_cubes; i++)
-    {
-        orientations->push_back(Quaternion(1.0f,  rand() % 5 - 2.0f, rand() % 5 - 2.0f, rand() % 5 - 2.0f ));
-        orientations->at(i).Normalise();
-    }
-    return orientations;
 }
 
 class WorldHandler {
@@ -93,65 +70,55 @@ public:
         AddEBlocks(num_e_blocks_1_2, 0.5f);
     }
 
-    void static GetProperties(int num_blocks, std::vector<Matrix> *&positions, std::vector<Quaternion> *&orientations, std::vector<Matrix> *&angular_momentums, std::vector<Matrix> * &linear_momentums) {
+    void static GetProperties(int num_blocks, std::vector<Matrix> *&positions, std::vector<Matrix> * &linear_momentums) {
         positions = GeneratePositions(num_blocks);
-        orientations = GenerateOrientations(num_blocks);
-        angular_momentums = GenerateAngularMomentums(num_blocks);
         linear_momentums = GenerateLinearMomentums(num_blocks);
     }
 
     void AddIBlocks(int num_i_blocks, bool state) {
-        std::vector<Matrix> *positions,  *angular_momentums, *linear_momentums;
-        std::vector<Quaternion> *orientations;
-        GetProperties(num_i_blocks, positions, orientations, angular_momentums, linear_momentums);
+        std::vector<Matrix> *positions, *linear_momentums;
+        GetProperties(num_i_blocks, positions, linear_momentums);
         for(int i=0; i<num_i_blocks; i++) {
             cout << "Add I Block ID: " << i << endl;
             auto new_block = new IBlock(positions->at(i), Quaternion(1.0, 0.0, 0.0, 0.0)  ,state);
             iblocks.push_back(new_block);
             blocks.push_back(new_block);
-            //iblocks.at(i)->SetAngularMomentum(angular_momentums->at(i));
             iblocks.at(i)->SetLinearMomentum(linear_momentums->at(i));
         }
     }
 
     void AddMBlocks(int num_m_blocks) {
-        std::vector<Matrix> *positions,  *angular_momentums, *linear_momentums;
-        std::vector<Quaternion> *orientations;
-        GetProperties(num_m_blocks, positions, orientations, angular_momentums, linear_momentums);
+        std::vector<Matrix> *positions,  *linear_momentums;
+        GetProperties(num_m_blocks, positions, linear_momentums);
         for(int i=0; i<num_m_blocks; i++) {
             cout << "Add M Block ID: " << i << endl;
             auto new_block =  new MBlock(positions->at(i), Quaternion(1.0, 0.0, 0.0, 0.0));
             mblocks.push_back(new_block);
             blocks.push_back(new_block);
-            //mblocks.at(i)->SetAngularMomentum(angular_momentums->at(i));
             mblocks.at(i)->SetLinearMomentum(linear_momentums->at(i));
         }
     }
 
     void AddEBlocks(int num_e_blocks, real k) {
-        std::vector<Matrix> *positions,  *angular_momentums, *linear_momentums;
-        std::vector<Quaternion> *orientations;
-        GetProperties(num_e_blocks, positions, orientations, angular_momentums, linear_momentums);
+        std::vector<Matrix> *positions, *linear_momentums;
+        GetProperties(num_e_blocks, positions, linear_momentums);
         for(int i=0; i<num_e_blocks; i++) {
             cout << "Add E Block ID: " << i << endl;
             auto new_block = new EBlock(positions->at(i), Quaternion(1.0, 0.0, 0.0, 0.0), k);
             eblocks.push_back(new_block);
             blocks.push_back(new_block);
-            //eblocks.at(i)->SetAngularMomentum(angular_momentums->at(i));
             eblocks.at(i)->SetLinearMomentum(linear_momentums->at(i));
         }
     }
 
     void AddZBlocks(int num_z_blocks) {
-        std::vector<Matrix> *positions,  *angular_momentums, *linear_momentums;
-        std::vector<Quaternion> *orientations;
-        GetProperties(num_z_blocks, positions, orientations, angular_momentums, linear_momentums);
+        std::vector<Matrix> *positions,  *linear_momentums;
+        GetProperties(num_z_blocks, positions, linear_momentums);
         for(int i=0; i<num_z_blocks; i++) {
             cout << "Add Z Block ID: " << i << endl;
             auto new_block = new ZBlock(positions->at(i), Quaternion(1.0, 0.0, 0.0, 0.0));
             zblocks.push_back(new_block);
             blocks.push_back(new_block);
-            //zblocks.at(i)->SetAngularMomentum(angular_momentums->at(i));
             zblocks.at(i)->SetLinearMomentum(linear_momentums->at(i));
         }
     }
