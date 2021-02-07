fb456572f487dcb9d33e722d86fe58c590a19684
Aamir <aamirsoni1551@gmail.com>
2021-02-07 17:14:12 +0000

    Add And Gate

---
+++ simulation/environment/and_gate_test.cpp
@@ -0,0 +1,140 @@
+#include <cstring>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <world_initializer.hpp>
+#include <settings.hpp>
+#include <matrix.hpp>
+#include <camera.hpp>
+#include <block_renderer.hpp>
+#include <world_handler.hpp>
+
+using namespace std;
+
+using namespace render_utils;
+using namespace numerics;
+using namespace settings;
+using namespace blocks;
+
+int main()
+{
+    cout << "Running And Logic Gates Simulation" << endl;
+
+    WorldProperties *world_properties = world_intializer();
+
+    unsigned int vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    real cube_length = 4.0f;
+    Camera camera = Camera(world_properties->window);
+    camera.camera_pos =  glm::vec3(-20, 0, 20);
+    BlockRenderer::InitialiseBlockRenderer(&camera, cube_length, vao, vbo, ebo, world_properties);
+
+    real down = -10;
+    // And Gate -- Given by 2 Inputs, Input 1 (A) = M block, Input 2 (B) = M block, Out (C) = M block :
+    // E1 C E2
+    // A    B
+    // I1   I2
+
+    WorldHandler world = WorldHandler(1, 0, 0, 3, 2, 0);
+
+    // I1
+    world.iblocks.at(0)->SetLinearMomentumToZero();
+    world.iblocks.at(0)->position = Matrix::CreateColumnVec(-22, down, -13);
+
+    // I2
+    //world.iblocks.at(1)->SetLinearMomentumToZero();
+    //world.iblocks.at(1)->position = Matrix::CreateColumnVec(-22, down, -5);
+
+    // A
+    world.mblocks.at(0)->SetLinearMomentumToZero();
+    world.mblocks.at(0)->position = Matrix::CreateColumnVec(-18, down, -13);
+
+    // B
+    world.mblocks.at(1)->SetLinearMomentumToZero();
+    world.mblocks.at(1)->position = Matrix::CreateColumnVec(-18, down, -5);
+
+    // E1
+    world.eblocks.at(0)->SetLinearMomentumToZero();
+    world.eblocks.at(0)->position = Matrix::CreateColumnVec(-14, down, -13);
+
+    // E2
+    world.eblocks.at(1)->SetLinearMomentumToZero();
+    world.eblocks.at(1)->position = Matrix::CreateColumnVec(-14, down, -5);
+
+    // C
+    world.mblocks.at(2)->SetLinearMomentumToZero();
+    world.mblocks.at(2)->position = Matrix::CreateColumnVec(-14, down, -9);
+
+
+
+    world.ResetTrees();
+
+    glBindVertexArray(vao);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glUseProgram(world_properties->shader_id);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //using black to clear the background
+    std::cout << "Entering Main Loop" << std::endl;
+
+    real deltaTime = 0.0f; // Time between current frame and last frame
+    real lastFrame = 0.0f; // Time of last frame
+    real currentFrame;
+
+    real frame_count = 0;
+    real prev_time = glfwGetTime();
+    while (!glfwWindowShouldClose(world_properties->window))
+    {
+
+        currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        camera.UpdateCamera(deltaTime);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        auto contact_list = world.CollisionHandler();
+        //world.AddForces(deltaTime);
+        world.Update(contact_list, deltaTime);
+
+        BlockRenderer::DrawAllBlocks(&world.iblocks, &world.zblocks, &world.eblocks, &world.mblocks);
+
+        glfwSwapBuffers(world_properties->window);
+        glfwPollEvents();
+        frame_count++;
+        if(currentFrame - prev_time >= 1.0) {
+            cout << "FPS: " << frame_count << endl;
+            frame_count = 0;
+            prev_time = currentFrame;
+
+            /*cout << "Flare value in I0 block: " << world.iblocks.at(0)->flare_value << endl;
+            cout << "Flare value in X1 block: " << world.mblocks.at(0)->flare_value << endl;
+            cout << "Flare value in X2 block: " << world.mblocks.at(1)->flare_value << endl;
+            cout << "Flare value in X3 block: " << world.eblocks.at(0)->flare_value << endl;*/
+
+            // Or Gate
+            cout << "Flare value in A block: " << world.mblocks.at(0)->flare_value << endl;
+            cout << "Flare value in B block: " << world.mblocks.at(1)->flare_value << endl;
+            cout << "Flare value in E1 block: " << world.eblocks.at(0)->flare_value << endl;
+            cout << "Flare value in E2 block: " << world.eblocks.at(1)->flare_value << endl;
+            cout << "Flare value in C block: " << world.mblocks.at(2)->flare_value << endl;
+
+        }
+    }
+
+    cout << "Terminating..." << endl;
+    glDeleteBuffers(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+
+    glfwTerminate();
+    cout << "Terminated" << endl;
+    return 0;
+}
